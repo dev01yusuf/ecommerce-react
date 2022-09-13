345affb8dca33644d55361a85074f0a183aba463
dev01yusuf <taoufikallahy@gmail.com>
2022-09-13 14:54:55 +0100

    adding css bootstrao in home

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,20 +3,25 @@ import React from "react";
 const Home = () => {
   return (
     <div className="hero">
-      <div class="card text-bg-dark">
-        <img src="/assets/bg.jpg" class="card-img" alt="background" height="550px"/>
-        <div class="card-img-overlay">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p class="card-text">
-            <small>Last updated 3 mins ago</small>
-          </p>
+      <div class="card text-bg-dark text-white border-0">
+        <img
+          src="/assets/bg.jpg"
+          class="card-img"
+          alt="background"
+          height="550px"
+        />
+        <div class="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="container">
+            <h5 class="card-title display-3 fw-bolder mb-40 text-black">
+              New Season Arrivals
+            </h5>
+            <p class="card-text lead fs-2 text-black">
+              Check out all the trends
+            </p>
+          </div>
         </div>
       </div>
-    </div> 
+    </div>
   );
 };
 
